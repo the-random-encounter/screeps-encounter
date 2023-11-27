4a724011e9b76457b9122b05de571127507b6dc7
the-random-encounter <31645891+the-random-encounter@users.noreply.github.com>
2023-11-26 23:43:14 -0600

    added remote running functionality

--- role.runner.js
+++ role.runner.js
@@ -23,15 +23,39 @@ const roleRunner = {
 		
 				if (!cMem.pickup && !cMem.dropoff) creep.assignLogisticalPair();
 
-				if (creep.store[cMem.cargo] == 0) {
-					const target = Game.getObjectById(cMem.pickup);
-					if (target) {
-						if (creep.withdraw(target, cMem.cargo) == ERR_NOT_IN_RANGE) creep.moveTo(target, { visualizePathStyle: { stroke: '#880088', opacity: 0.3, lineStyle: 'dotted' } });
+				if (cMem.cargo === undefined) cMem.cargo = 'energy';
+				if (cMem.dropoff == 'none') if (room.storage) cMem.dropoff = room.storage.id;
+
+				if (creep.store[RESOURCE_ENERGY] == 0 || creep.store[cMem.cargo] == 0) {
+					if (cMem.pickup == 'none') {
+						let piles = creep.room.find(FIND_DROPPED_RESOURCES);
+						piles = piles.sort((a, b) => b.amount - a.amount);
+
+						if (piles.length > 0) {
+							if (creep.pickup(piles[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE)
+								creep.moveTo(piles[0], { visualizePathStyle: { stroke: '#880088', opacity: 0.3, lineStyle: 'dotted' } });
+						}
+					} else {
+						const target = Game.getObjectById(cMem.pickup);
+						if (target) {
+							if (creep.withdraw(target, cMem.cargo) == ERR_NOT_IN_RANGE) creep.moveTo(target, { visualizePathStyle: { stroke: '#880088', opacity: 0.3, lineStyle: 'dotted' } });
+						}
 					}
 				} else {
 					const target = Game.getObjectById(cMem.dropoff);
 					if (target) {
-						if (creep.transfer(target, cMem.cargo) == ERR_NOT_IN_RANGE) creep.moveTo(target, { visualizePathStyle: { stroke: '#880088', opacity: 0.3, lineStyle: 'dotted' } });
+						if (pos.isNearTo(target)) {
+							if (target.store.getFreeCapacity(RESOURCE_ENERGY) > 0) creep.transfer(target, RESOURCE_ENERGY);
+						}
+						else {
+							if (creep.getActiveBodyparts(WORK) > 0) {
+								const roadUnderCreep = room.find(FIND_STRUCTURES, { filter: (i) => (i.structureType == STRUCTURE_ROAD && i.pos.x == pos.x && i.pos.y == pos.y && i.hits !== i.hitsMax) })
+								const roadTarget = pos.findClosestByRange(roadUnderCreep);
+								if (roadUnderCreep.length > 0) creep.repair(roadUnderCreep[0]);
+								else creep.moveTo(target, { visualizePathStyle: { stroke: '#880088', opacity: 0.3, lineStyle: 'dotted', ignoreCreeps: true } });
+							}
+							else creep.moveTo(target, { visualizePathStyle: { stroke: '#880088', opacity: 0.3, lineStyle: 'dotted', ignoreCreeps: true } });
+						}
 					}
 				}
 			} else { // I HAVE A RALLY POINT, LET'S BOOGY!
